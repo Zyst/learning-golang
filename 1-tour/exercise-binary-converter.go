d63ca56c75a765882f379036fb467182a6cb2a16
Zyst <zyst@gmx.com>
2016-11-26 22:14:00 -0600

    I'll restart this later, my brain is mush

--- 1-tour/exercise-binary-converter.go
+++ 1-tour/exercise-binary-converter.go
@@ -7,32 +7,17 @@ import (
 	"math"
 )
 
+// I'll come back to this later, probably
 func binaryConverter(decimal int) (binary []byte, err error) {
 	if decimal < 0 {
 		return nil, errors.New("binaryConverter: Can't convert negative numbers")
 	}
 
-	power := 0
-
-	// First we find out how many "Powers of 2" we need
-	// to fit in the passed decimal
-	for {
-		if math.Pow(2, float64(power)) > float64(decimal) {
-			break
-		}
-		power++
-	}
-
-	result := make([]byte, power)
-
-	// Restarting the meat of the for, I think I'll only grow more confused otherwise
-	// When the soil is salted, move.
-
 	return result, nil
 }
 
 func main() {
-	test, err := binaryConverter(10)
+	test, err := binaryConverter(16)
 
 	if err != nil {
 		log.Fatal(err)
