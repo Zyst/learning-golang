05d8d1d8c9d54128da04c5d2f363c374b7e5268a
Zyst <zyst@gmx.com>
2016-11-30 21:15:08 -0600

    Same implemented, figuring out how to do walk

--- 1-tour/exercise-equivalent-binary-trees.go
+++ 1-tour/exercise-equivalent-binary-trees.go
@@ -1,17 +1,24 @@
 package main
 
-package main
-
 import "golang.org/x/tour/tree"
 
 // Walk walks the tree t sending all values
 // from the tree to the channel ch.
-func Walk(t *tree.Tree, ch chan int)
+func Walk(t *tree.Tree, ch chan int) {
+
+  // Do this somehow?
+  ch <- t.Value
+}
 
 // Same determines whether the trees
 // t1 and t2 contain the same values.
-func Same(t1, t2 *tree.Tree) bool
+func Same(t1, t2 *tree.Tree) bool {
+  return t1.Value == t2.Value
+}
 
 func main() {
+  ch := make(chan int)
+
+  go Walk(tree.New(1), ch)
 }
 
