966f4cb16b8e0e1577f6edcbd52360473d8950d5
Zyst <zyst@gmx.com>
2016-11-30 23:16:33 -0600

    I'm fairly ashamed about a lot of this code, but god damnit I did it! I'll google a 100 times better solution...

--- 1-tour/exercise-equivalent-binary-trees.go
+++ 1-tour/exercise-equivalent-binary-trees.go
@@ -14,15 +14,47 @@ func Walk(t *tree.Tree, ch chan int) {
 	if t.Right != nil {
 		Walk(t.Right, ch)
 	}
-	// This is gonna run regarldess
+	// This sends values to the tree!
 	ch <- t.Value
 }
 
 // Same determines whether the trees
 // t1 and t2 contain the same values.
 func Same(t1, t2 *tree.Tree) bool {
-	// This actually needs to use Walk, so no cigar
-	return t1.Value == t2.Value
+
+	// We make two channels to walk trees
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+
+	// We send both trees to walk
+	go Walk(t1, ch1)
+	go Walk(t2, ch2)
+
+	firstCompare, secondCompare := WalkedTreeToArray(ch1), WalkedTreeToArray(ch2)
+
+	// Can't do this with a single loop because items can be in different orders,
+	for _, value := range firstCompare {
+		for i := range secondCompare {
+			if secondCompare[i] == value {
+				// We "Delete" this value since it has matched once
+				secondCompare = append(secondCompare[:i], secondCompare[i+1:]...)
+				break
+			}
+		}
+	}
+
+	fmt.Println(firstCompare, secondCompare)
+
+	// If we have 0 elements left they are the same
+	return len(secondCompare) == 0
+}
+
+func WalkedTreeToArray(ch chan int) []int {
+	result := make([]int, 10)
+	for index := 0; index < 10; index++ {
+		result[index] = <-ch
+	}
+	return result
 }
 
 func main() {
@@ -30,7 +62,10 @@ func main() {
 
 	go Walk(tree.New(1), ch)
 
-	for index := 0; index < 10; index++ {
-		fmt.Println(<-ch)
-	}
+	fmt.Println(WalkedTreeToArray(ch))
+
+	fmt.Println("\n\n")
+
+	fmt.Println("Expect true: ", Same(tree.New(1), tree.New(1)))
+	fmt.Println("Expect false: ", Same(tree.New(1), tree.New(2)))
 }
